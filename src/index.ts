d5c2c6a6b5b9bfeec5c0759a62e079e2b5638c9e
Areeb ur Rub <areeburrub@gmail.com>
2024-10-27 17:58:35 +0530

    fix: workflow to use nvm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "./server";
 
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT || "5000");
 
 app.route("/health").get((req, res) => {
   res.send("OK");
 });
 
-app.listen(port, () => {
+app.listen(port, 'localhost', () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
