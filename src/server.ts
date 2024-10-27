1bcc2891a62717051f43ddb1ffb8dcdc78954d96
Areeb ur Rub <areeburrub@gmail.com>
2024-10-27 16:52:19 +0530

    fix: CORS from env

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 // enable cors
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
   })
